98b0eca749937739a580d2b802fcf62feb5756fc
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-03-09 21:17:53 -0700

    Apply Changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import Home from "./components/Home";
 import Login from "./components/Login";
+import NavigationBar from "/components/NavigationBar";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
