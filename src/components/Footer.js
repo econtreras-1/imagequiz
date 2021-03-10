a1d22f5d5216226516025956d1cde96569a2f578
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-03-09 21:02:44 -0700

    Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+
+
+function Footer(){
+    return (
+        <div>&copy; Copyright {new Date().getFullYear()}, https://econtreras-1.github.io/ , All rights reserved.</div>
+    );
+}
+
+
+export default Footer;
