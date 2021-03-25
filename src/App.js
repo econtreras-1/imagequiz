a6288365eb76acd36a3176273cacaf81a85d5ca6
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-03-24 19:58:13 -0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import { HashRouter, Switch, Route } from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
+import {useState} from 'react';
+
 import Container from "react-bootstrap/Container";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Quiz from './components/Quiz';
 
 
 
 function App() {
+  
+  const history = useHistory();
+  const [username, setUsername] = useState(localStorage.getItem('username')|| '');
+  const [quizNumber, setQuizNumber] = useState(localStorage.getItem('quizID')|| 0);
+  
+  let loggedIN = (email) => {
+    localStorage.setItem('username', email);
+    setUsername(email);
+  }
+
+  let quizClicked = (quizID) => {
+    localStorage.setItem('quizID', quizID);
+    setQuizNumber(quizID);
+  }
+  
   return (
     <HashRouter>
       <Header />
       <Container fluid>
-      <NavigationBar />
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/login" component={Login}/>
-      </Switch>
+        <NavigationBar username={username} />
+        <Switch>
+          <Route exactpath="/">
+            <Home quizClicked={quizClicked} />
+          </Route>
+          <Route path="/login">
+            <Login loggedIN={loggedIN} />
+          </Route>
+          <Route path='/quiz'>
+            <Quiz quizNumber={quizNumber}/>
+          </Route>
+        </Switch>
       </Container>
       <Footer />
     </HashRouter>
