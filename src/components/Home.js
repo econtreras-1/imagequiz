7a7ba11f30c2cf88844b1cd95f6ece7161f37fd3
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-03-24 20:51:08 -0700

    Commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,34 +6,33 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function Home(props) {
 
-    let imageClicked = (event) => {
-        props.quizClicked(event.target.id);
+    let onImageClick = (event) => {
+        props.onQuizClicked(event.target.id);
     }
 
     return (
         <div>
-            <h2 style={{textAlign: "center"}}>Home</h2>
+            <h2 style={{ textAlign: "center" }}>Home</h2>
             {createFlowers()}
         </div>
     );
-}
 
-function createFlowers() {
-    let images = [];
-    let i = 0;
-    for (let flwr in flowers) {
-        images.push(
-            <Link id={i} onClick={imageClicked} className='link' to='/quiz'>
-                <Figure>
-                    <Figure.Image width={171} height={180}
-                        alt={flowers[flwr].name}
-                        src={flowers[flwr].picture} />
-                    <Figure.Caption>{flowers[flwr].name}</Figure.Caption>
+
+    function createFlowers() {
+        let images = [];
+        let i = 0;
+        for (let flw in flowers) {
+            images.push(<Link id={i} onClick={onImageClick} className='link' to="/quiz">
+                <Figure id={i}>
+                    <Figure.Image id={i} width={171} height={180}
+                        alt={flowers[flw].name}
+                        src={flowers[flw].picture} />
+                    <Figure.Caption id={i}>{flowers[flw].name}</Figure.Caption>
                 </Figure>
             </Link>)
             i++;
+        }
+        return images;
     }
-    return images;
-}
 
-export default Home;
+}
