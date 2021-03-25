3b46e56f595584c94acbadcb8150186874a5c6d6
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-03-24 20:30:02 -0700

    Commit

---
+++ src/components/QuizPopup.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Figure from 'react-bootstrap/Figure';
+import flowerQuiz from '../flowerQuiz';
+
+
+export default function QuizPopup(props) {
+    const [answer, setAnswer] = useState("");
+    const [btnState, setBtnState] = useState(false);
+
+    let answerGiven = (event) => {
+        setBtnState(true);
+        if(flowerQuiz[props.quizNumber][props.questionNumber].choices[event.target.id] === flowerQuiz[props.quizNumber][props.questionNumber].answer) {
+            setAnswer("You answered "+ flowerQuiz[props.quizNumber][props.questionNumber].answer+ " and got it right!");
+            props.answerGiven(true);
+        } else {
+            setAnswer("You got it wrong. Correct answer is: "+ flowerQuiz[props.quizNumber][props.questionNumber].answer);
+            props.answerGiven(false);
+        }
+    }
+    
+    return (
+        <Row><Col>
+            <Figure>
+                <Figure.Image width={171} height={180}
+                    alt={flowerQuiz[props.quizNumber][props.questionNumber].answer}
+                    src={flowerQuiz[props.quizNumber][props.questionNumber].picture} />
+                <Figure.Caption>
+                    <Button disabled={btnState} id={0} onClick={answerGiven}>{flowerQuiz[props.quizNumber][props.questionNumber].choices[0]}</Button>
+                    <Button disabled={btnState} id={1} onClick={answerGiven}>{flowerQuiz[props.quizNumber][props.questionNumber].choices[1]}</Button>
+                    <Button disabled={btnState} id={2} onClick={answerGiven}>{flowerQuiz[props.quizNumber][props.questionNumber].choices[2]}</Button>
+                    <br/>{answer}                   
+                </Figure.Caption>
+            </Figure>
+
+        </Col></Row>
+    );
+}
