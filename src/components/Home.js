8c20d8ee8d572e2677288f766053d455f80e2c4d
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-05-02 21:20:20 -0700

    Push

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import Figure from 'react-bootstrap/Figure';
 import 'bootstrap/dist/css/bootstrap.css';
 import { useState, useEffect } from 'react';
-import api from "../communication/api"
+import api from "../communication/api";
 
 
 function Home(props) {
