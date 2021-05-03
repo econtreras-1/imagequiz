f26c565009d7a665241b27d158bf442c78b82ac0
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-05-02 21:15:02 -0700

    Created API

---
+++ src/communication/api.js
@@ -0,0 +1,30 @@
+let apiHost = "https://imagequiz-serverside.herokuapp.com";
+
+let getFlowers = () => {
+    return fetch(apiHost + '/flowers')
+    .then(response => response.json());
+};
+
+let getQuizzes = () => {
+    return fetch(apiHost + '/quizzes')
+    .then(response => response.json());
+};
+
+let addScore = (score) => {
+    return fetch(apiHost + '/score', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(score)
+    });
+}
+
+
+let api = {
+    getFlowers: getFlowers,
+    getQuizzes: getQuizzes,
+    addScore: addScore
+};
+
+export default api;
