13efc44f3838175b28ce89aa38c3f0fe7c1e90d3
econtreras-1 <77944085+econtreras-1@users.noreply.github.com>
2021-05-02 21:19:05 -0700

    useEffect Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import Figure from 'react-bootstrap/Figure';
 import 'bootstrap/dist/css/bootstrap.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import api from "../communication/api"
 
 
